14541d0871d56cf9708b17ee82d4f70ef6662541
Juan Carlos <taraguipora2005@gmail.com>
2025-05-07 22:13:22 -0300

    Refactor app structure and styles
    - Updated app.component.html to remove placeholder content and retain only necessary components.
    - Modified app.component.ts to include necessary imports and configure standalone component with required modules.
    - Enhanced app.config.ts to include HTTP client provider for API interactions.
    - Updated index.html to include Bootstrap CSS for styling.
    - Refactored main.ts to ensure HTTP client is provided during application bootstrap.
    - Improved global styles in styles.css for better layout and sidebar configuration.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from './shared/shared.module';
+import { GifsModule } from './gifs/gifs.module';
+import { SideBarComponent } from './shared/side-bar/side-bar.component'; 
+import { ResultadosComponent } from './gifs/resultados/resultados.component';
+import { GifsService } from './gifs/services/gifs.service';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule,SharedModule,SideBarComponent,GifsModule,],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  providers: [GifsService],  
 })
 export class AppComponent {
   title = 'giftApp';
